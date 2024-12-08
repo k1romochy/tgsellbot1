34742b62287ab9eeb6dfc09f1a5ffef4bfd0cac0
k1romochy <k1romochy@mail.ru>
2024-12-08 16:13:59 +0300

    first

--- app/main.py
+++ app/main.py
@@ -1,23 +1,20 @@
-import asyncio
-from aiogram import Bot, Dispatcher
+from aiogram import Dispatcher
 from dotenv import load_dotenv
 import os
+import asyncio
 
 from core.models.db_helper import async_main
-from user.handler import router
-from product.handler import router as router2
+from user.handler import router as user_router
+from product.handler import router as product_router
+from context import bot
 
 
 async def main():
-    load_dotenv()
-    bot = Bot(token=os.getenv('TOKEN'))
     dp = Dispatcher()
-    dp.include_router(router)
-    dp.include_router(router2)
+    dp.include_router(user_router)
+    dp.include_router(product_router)
     await async_main()
-    await dp.start_polling((bot))
+    await dp.start_polling(bot)
 
 if __name__ == '__main__':
     asyncio.run(main())
-
-
--- user/handler.py
+++ user/handler.py
@@ -1,12 +1,10 @@
-from aiogram import F, Router
-from aiogram.types import Message, CallbackQuery
-from aiogram.filters import Command, CommandStart
-from sqlalchemy.ext.asyncio import AsyncSession
+from aiogram import Router
+from aiogram.types import Message
+from aiogram.filters import CommandStart, Command
 
 import user.crud as user
-import aiosqlite
 from markups.main import main
-import product.crud as product
+from app.context import bot
 
 router = Router()
 
@@ -15,13 +13,21 @@ router = Router()
 async def get_user_id_first_mess(message: Message):
     await user.create_user(tg_id=message.from_user.id)
     await message.answer(text='Добро пожаловать!', reply_markup=main)
-
     if message.from_user.id == 5470849504:
-        await message.answer(f'Вы авторизовались как администратор \nВведите /create_product чтобы добавить товар')
+        await message.answer(
+            'Вы авторизовались как администратор \nВведите /create_product чтобы добавить товар'
+        )
 
 
-@router.message(F.text == 'Профиль')
-async def get_user_profile(message: Message):
-    balance = await user.get_user_balande(message.from_user.id)
-
-    await message.reply(f'Баланс: {balance}', reply_markup=main.profile)
+@router.message(Command('sendmess'))
+async def send_message_to_user(message: Message):
+    if message.from_user.id == 5470849504:
+        args = message.text.split(' ', 2)
+        if len(args) < 3:
+            await message.answer('Используйте формат: /sendmess user_id текст')
+            return
+        user_id = int(args[1])
+        text = args[2]
+        await bot.send_message(chat_id=user_id, text=text)
+    else:
+        await message.answer('Вы не обладаете такими правами')
